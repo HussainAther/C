8b1e2c010525d13479602a6a2c2250a4e20970e7
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 17:32:09 -0500

    initial commit

--- over.c
+++ over.c
@@ -1,1 +1,16 @@
 // over.c: Determine overflow and underflow limits
+
+#include <stdio.h>
+#define N 1024 // i wonder if this is big enough to cause over and underflow
+
+main() {
+
+    doouble under =1, over =1;
+    int i;
+    
+    for (i=0; i<1024; i++) {
+        under /= 2;
+        over *= 2;
+        printf("%d. under: %e over: %e \n" i+1, udner, over);
+    }
+}
