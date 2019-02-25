297361bca91650eaf64443b6da4b8b9ae5bb2e5b
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:13:38 -0500

    comment

--- fourier.c
+++ fourier.c
@@ -1,5 +1,9 @@
 // fourier.c: Discrete Fourier Transformation
 /*
-This program reads 
+This program reads its input data from a file in the same directory
+called input.dat. This has to contain only y(t) values separated by whitespaces which
+are real. The output is the direct output from the algorithm which will probably
+look very different than what you are used to seeing. The output has the form
+frequency index \t real part \t imaginary part
 /**/
 
