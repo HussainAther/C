d2e4ddd69e79b53e55951258a65d70127232cb19
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 16:55:15 -0500

    define include

--- int10d.c
+++ int10d.c
@@ -1,1 +1,9 @@
 // int10d.c: ten-dimensional Monte Carlo integration
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+// if you don't haev drand48, uncomment hte following two lines
+    #define drand48 1.0/RAND_MAX*rand
+    #define srand48 srand
+#define max 65536 //number of trials
