833594986ca199a5b5ac8b0f13c888470fe7245f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:15:28 -0500

    let's get to it

--- fourier.c
+++ fourier.c
@@ -7,3 +7,16 @@ look very different than what you are used to seeing. The output has the form
 frequency index \t real part \t imaginary part
 /**/
 
+#include <stdio.h>
+#include <math.h>
+#define max 1000
+#define PI 3.1415926535897932385
+
+main() {
+
+    double imag, real, input[max+1];
+    int i =0,j,k;
+    FILE *data;
+    FILE *output; 
+    
+}
