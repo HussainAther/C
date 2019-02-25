324d8a43e93deac4b9de1e95e4e2a98759bc9cf1
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 02:01:50 -0500

    simpson

--- integ.c
+++ integ.c
@@ -32,3 +32,36 @@ main() {
     printf("data stored in integ.dat\n");
     fclose(output);
 }
+
+float f (float x) {
+    return (exp(-x)) // function to integrate
+}
+
+float trapez (int no, float min, float max) { // trapzeoid rule
+    
+    int n;
+    float interval,
+    sum = 0, x;
+    interval = ((max-min)/ (no-1));
+    for (n-2; n<no; n++) { // sum the midpoints
+        x = interval * (n-1)
+        sum += f(x)*interval;
+    }
+    sum += .5 *(f(min) + f(max)) * interval;
+    return (sum);
+}
+
+float simpson (int no, float min, float max) { // Simpson's rule
+    
+    int n;
+    float interval, sum =0, x;
+    interval = ((max - min)/ (no-1));
+    for (n=2; n<no; n+=2) {
+        x = interval (n-1);
+        sum += 2 * f(x);
+    }
+    sum += f(min) + f(max);
+    sum *= interval/3;
+    return (sum)
+}
+
