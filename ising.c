b639c42ede86177c734541bdabe3c2ece93a0e65
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 16:59:45 -0500

    initial commit

---
+++ ising.c
@@ -0,0 +1,12 @@
+// Ising.c: ising model of magnetic dipole string
+// Plot without conecting datapoints with lines
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// if you don't have drand48, uncomment the following two lines
+    #define drand48 1.0/RAND_MAX*rand
+    #define srand48 srand
+
+#define max 100 // number of objects
