070042ddb99404f612fd5954ee511d4070505875
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:34:51 -0500

    intial commit

---
+++ integ.c
@@ -0,0 +1,3 @@
+// integ.c: integration using trapezoid, Simpson, and Gauss rules.
+// The derivation from theoretical results for each method
+// is saved in x, y1, y2, y3 format. Needs to be in same directory as gauss.c
