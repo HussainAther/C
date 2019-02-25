8ea3c3787365b1b69443554d6eb13f80ec848dd6
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 17:09:18 -0500

    initial commit

---
+++ lineq.c
@@ -0,0 +1,10 @@
+// lineq.c: SOlve matrix equation Ax = b using LAPACK
+
+#include<stdio.h>
+#define size 3 // matrix dimension
+
+main() {
+    
+    int i, j, c1, c2, pivot[size], ok;
+    float A[size][size], b[size], AT[size*size];
+}
