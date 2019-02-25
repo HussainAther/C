e75abda380742fe8c20193218e593f3d187e37db
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 00:22:16 -0500

    some of it

--- eqheat.c
+++ eqheat.c
@@ -7,3 +7,20 @@
 #define thc .12 // thermal conductivity
 #define sph .113 // specific heat
 #define rho 7.8 // density
+
+main() {
+    
+    int i, j;
+    double cons, u[101][2];
+    
+    FILE *output // save data to eqheat.dat
+    output = fopen("eqheat.dat", "w")
+    
+    for (i = 0; i<size; i++) u[i][0] = 100;
+    
+    for (j = 0; j<2; j++) {
+        u[0][j] = 0;
+        u[size-1][j] = 0;
+    }
+    
+}
