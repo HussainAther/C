22795879a92e8401f2e2f759170f166a7db57d68
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 00:28:55 -0500

    define

--- eqstring.c
+++ eqstring.c
@@ -1,2 +1,9 @@
 // eqstring.c: Solution of wave equation using time stepping
 // saves output to 3D grid format used by gnuplot
+
+#include <stdio.h>
+#include <math.h>
+#define rho .01 // density per length
+#define ten 40 // tension
+#define max 100 // time steps
+
