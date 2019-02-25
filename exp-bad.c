2509daa11df0155a2ce65d0aac538ef6327aebe7
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 00:37:54 -0500

    define

--- exp-bad.c
+++ exp-bad.c
@@ -1,3 +1,7 @@
 // exp-bad.c: a bad algorithm for calculating exponential
 // related programs
 
+#include <stdio.h>
+#include <math.h>
+#define min 1E-10 // limit for accuracy
+#define max 10 // maximum for x
