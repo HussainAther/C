d3e168d4caf2723bc674c8b387846414f8b6a005
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:38:11 -0500

    double

--- gauss.c
+++ gauss.c
@@ -8,3 +8,7 @@ void gauss(int npts, int job, double a, double b, double x[\, double w[])
 //          1. for integral (0,b) with 50% pts inside (0, ab/(a+b))
 //          2. for integral (a,inf) with 50% inside (a, b+2a)
 // x, w output grid points and weights
+
+    int m, i, j;
+    double t, t1, pp, p1, p2, p3;
+    double pi = 3.1415926535897932385E0, eps = 3e-10 // accuracy
