7f1b23daf76509f839ef8260c4e514fcb256c9e1
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 00:20:48 -0500

    define more

--- eqheat.c
+++ eqheat.c
@@ -3,3 +3,7 @@
 #include <stdllib.h>
 #include <stdio.h>
 #define size 101 // grid size
+#define max 30000 // iterations
+#define thc .12 // thermal conductivity
+#define sph .113 // specific heat
+#define rho 7.8 // density
