6c9af380a816c77d436e6b7eabeb403822dbe8e1
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:58:04 -0500

    data write

--- integ.c
+++ integ.c
@@ -5,10 +5,10 @@
 #include <stdio .h>
 #include <math.h>
 #include "gaus.c"
-#define max_in 501
-#define vmin 0.0
+#define max_in 501 // max number of intervals
+#define vmin 0.0 // ranges of integration
 #define vmax 1.0
-#define ME 2.7182818284590452354E0
+#define ME 2.7182818284590452354E0 // Euler's constant
 main() {
 
     int i;
@@ -21,4 +21,14 @@ main() {
     FILE *output;
     output = fopen("integ.dat", "w");
     
-    
+    for (i=3; i<= max_in; i+=2) {
+        result = trapez(i, vmin, vmax);
+        fprintf(output, "%i\t%e\t", i, fabs(result-1+1/ME);)
+        result = simpson(i, vmin, vmax);
+        fprintf(output, "%i\t%e\t", fabs(result-1+1/ME);)
+        result = gaussint(i, vmin, vmax);
+        fprintf(output, "%i\t%e\t", fabs(result-1+1/ME);)
+    }
+    printf("data stored in integ.dat\n");
+    fclose(output);
+}
