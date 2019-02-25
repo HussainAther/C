72131b30ed50ea52c0c223a9d98ff7b3268abf00
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:45:10 -0500

    define

--- bound.c
+++ bound.c
@@ -4,3 +4,22 @@
  eigenvalue is printed to standard output and
  the corresponding eigenvector to the file bound.dat.
 */
+
+#include <stdio.h>
+#include <math.h>
+#include "gauss.c"
+
+#define min 0
+#define max 200
+#define size 64
+#define lambda -1
+#define u .5
+#define b 2
+#define PI 3.1415926535897932385E0
+
+main() {
+
+    int i, j, c1, c2, c5, ok;
+    char c3, c4;
+    
+}
