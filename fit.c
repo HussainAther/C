71b97b21b16b840030cf4e65847e26f1e5b8d34b
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:01:49 -0500

    initial commit

---
+++ fit.c
@@ -0,0 +1,21 @@
+// fit.c: least-squares fit
+
+#include <stdio.h>
+#include <math.h>
+#define data 12 // number of data points
+
+main() {
+
+    int i, j;
+    double s, sx, sy, sxx, sxy, delta, inter, slope;
+    double x[data], y[data], d[data];
+    
+    for (i=0; i<data; i++)
+        x[i] = i*10+5;
+        y[0] = 382;
+        y[1] = 187;
+        y[2] = 281;
+        y[3] = 87;
+        y[4] = 8;
+        y[5]] = 6
+}
