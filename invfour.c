738d64a1703e22d529a6fd1dce2d1e04a22414f6
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 16:49:21 -0500

    initial commit

---
+++ invfour.c
@@ -0,0 +1,8 @@
+// invfour.c: Inverse Discrete Fourier Transformation
+/* This program reads its input data from a file in the smae directory fourier.dat
+* which must have the same format (frequency index real part imaginary part) as created
+* with the program fourier.c. The output has the same format.
+*/
+
+#include <stdio.h>
+#include <math.h>
