4596ddf908c03c175ff9fbb0b856698e88d63dbe
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 00:51:26 -0500

    done

--- film.c
+++ film.c
@@ -8,3 +8,23 @@
     #define srand48 srand
 #define max 30000 // number of iterations
 #define seed 68111 // seed for number generator
+
+main() {
+    
+    int i, hit[200], r;
+    
+    FILE *output;
+    output = fopen("film.data", "w");
+    srand48(seed);
+    
+    for (i=0; i<200; i++) hit[i] = 0;
+    for (i=1; i<=max; i++) {
+        r=(int)(199*drand48());
+        if ((hit[r] >=hit[r-1]) && (hit[r] >= hit[r+1])) hit[r]++;
+        else if (hit[r-1] > hit[r+1]) hit[r] = hit[r-1];
+        else hit[r] = hit[r+1];
+        fprintf(output, "%d\t%d\n", r, hit[r]);
+    }
+    printf("data stored in film.dat\n");
+    fclose(output);
+}
