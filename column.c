6dec089776b317478fac75f56e4472cf884b9bca
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 16:46:02 -0500

    if else

--- column.c
+++ column.c
@@ -18,6 +18,26 @@ main() {
     output = fopen("column.dat", "w")
     srand48(seed); // seed rng
     for (i=0; i<npoints; i++) hit[i] = 0; // clear the array
-    
+    oldx = 100;
+    oldy = 0;
+    for (i=1; i<=max; i++) {
+        r = (int) (npoints*drand48());
+        x = r-oldx;
+        y = hit[i]-oldy;
+        dist = x*x + y*y;
+        if (dist == 0) prob = 1.0;
+            else prob = 9.0/(dist);
+        pp = drand48();
+        if (pp < prob) {
+            if ((r>0) && (R<(npoints-1))) {
+                if ((hit[r] >= hit[r-1]) && (hit[r] >= hit[r+1])) hit[r]++;
+                    else if (hit[r-1] > hit[r+1]) hit[r] = hit[r-1];
+                        else hit[r] = hit[r+1];
+            oldx = r;
+            oldy = hit[r];
+            fprintf(output, "%d\t%d\n", r, hit[r])
+            }
+        }
+    }
     
 }
