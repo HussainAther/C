1c694e463259baa02d28238ca6c5f10048ebdd3e
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 16:39:59 -0500

    define

--- column.c
+++ column.c
@@ -1,1 +1,12 @@
 // column.c: Correlated ballistic deposition to form fractal
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// if you don't ahve drand48, uncomment the following two lines
+    #define drand48 1.0/RAND_MAX*rand
+    #define srand48 srand
+
+#define max 100000 // iterations
+#define npoints 200 // no. of open spaces
+#define seed 6811
