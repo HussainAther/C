afa89bab881d01cb73e67ac4ca46b1fbc8647b11
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 00:35:24 -0500

    done

--- eqstring.c
+++ eqstring.c
@@ -7,3 +7,32 @@
 #define ten 40 // tension
 #define max 100 // time steps
 
+main() {
+
+    int i, k;
+    double x[101][3];
+    
+    FILE *out;
+    out = fopen("eqstring.dat", "w")
+    
+    for (i = 0; i<81; i++)
+        x[i][0] = .00125;
+    for (i = 81; i<101; i++)
+        x[i][0] = .1 - .005*(i-80);
+    for (i = 1; i<100; i++)
+         x[i][1] = x[i][0] + .5*(x[i+1][0] + x[i-1][0] - 2*x[i][0]);
+    for (k = 1; k<max; k++)
+        {for (i=1; i<100; i++)
+            x[i][2] = 2*x[i][1]-x[i][0]+(x[i+1][1]+x[i-1][1]-2*x[i][1]);
+            for (i=0; i<101; i++){
+                x[i][0] = x[i][1];
+                x[i][1] = x[i][2];
+            }
+        if ((k%5) ==0) {
+            for (i =0; i<101; i++) {// print every 5th point
+                fprintf(out, "%f\n", x[i][2]); // empty line for gnuplot
+            }
+        }
+    printf("data stored in eqstring.dat\n");
+    fclose(out)
+}
