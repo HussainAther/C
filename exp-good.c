0e45c7b5cb76d88f28ae2d6100acf76e14777f43
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 00:47:06 -0500

    done

--- exp-good.c
+++ exp-good.c
@@ -1,2 +1,29 @@
 // exp-good.c: good algorithm for calculating exponential
 // related programs
+
+#include <stdio.h>
+#include <math.h>
+#define min 1E-10 // limit for accuracy
+#define max 10 // maximum for x
+#define step .1 // interval
+
+main () {
+
+    double x, sum, element;
+    int n;
+    
+    FILE *output;
+    output = fopen("exp-good.dat", "w");
+    for (x = 0; x <= max; x += step) {
+        sum = element = 1;
+        n = 0;
+        do {n++;
+            element *= -x/n;
+            sum += element;
+        }
+        while ((sum ==0) || (fabs(element/sum) > min));
+            fprintf(output, "%f\t%e\n", x, sum);
+    }
+    printf("results saved in exp-good.dat\n");
+    fclose(output)
+}
