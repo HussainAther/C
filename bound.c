12f165ebe13660c34c1467d1aa9751f7dcac5693
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 01:32:24 -0500

    intial commit

---
+++ bound.c
@@ -0,0 +1,6 @@
+// bound.c: bound states in momentum space of delta shell potential
+/* using the LAPACK dgeev and Gaussian integration. LAPACK has to be installed
+ on your system and the file gauss.c has to be in the same directory. The energy
+ eigenvalue is printed to standard output and
+ the corresponding eigenvector to the file bound.dat.
+*/
