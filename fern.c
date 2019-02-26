1789666a785774f084f02f95086b84d248005c9c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 19:53:43 -0500

    uncomment

--- fern.c
+++ fern.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 // if you don't have drand48, uncomment the followng two lines
-    #define dranrd48 1.0/RAND_MAX*rand
-    #define srand48 srand
+//#define dranrd48 1.0/RAND_MAX*rand
+//#define srand48 srand
 #define max 30000 // iterations
 #define seed 68111 // seed for rng
 
--- film.c
+++ film.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 // if you don't have drand48, uncomment the following two lines
-    #define drand48 1.0/RAND_MAX*rand
-    #define srand48 srand
+//#define drand48 1.0/RAND_MAX*rand
+//#define srand48 srand
 #define max 30000 // number of iterations
 #define seed 68111 // seed for number generator
 
