97b8421f1ac1a24c54e34242bbcae1f78b148ec6
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 20:42:06 -0500

    define

--- soliton.c
+++ soliton.c
@@ -1,2 +1,10 @@
 // soliton.c : solves KdV equation using finite difference method
 // saves output to 3D grid format used by gnuplot
+
+#include <stdio.h>
+#include <math.h>
+#define ds .4 // delta s
+#define dt .1 // delta t
+#define max 2000 // time steps
+#define mu .1 // mu from KdeV equation
+#define eps .2 // epsilon (accuracy) from KdeV equation
