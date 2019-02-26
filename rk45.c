4183f464f54b190a79f4a2282c18f22e5c3296cd
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 20:20:01 -0500

    doubles

--- rk45.c
+++ rk45.c
@@ -11,5 +11,16 @@ main() {
     
     double h, t, s, s1, hmin, hmax;
     double y[2], fReturn[2], ydumb[2];
+    double k1[2], k2[2], k3[2];
+    double k4[2], k5[2], k6[2];
+    double err[2];
+    double Tol = 1.0E-8; // error control tlerance
+    double a = 0; // endpoints in calculation
+    double b = 10;
+    int i, j, n = 20;
+    void f(double t, double y[], double fReturn[]);
+    
+    y[0] = 1; y[1] = 0;
+    h = (b-a)/n
     
 }
