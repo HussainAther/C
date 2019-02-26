792a70d8125ed5460200d5c9762cd02bfebd9600
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 20:18:33 -0500

    main

--- rk45.c
+++ rk45.c
@@ -1,2 +1,15 @@
 // rk45.c: Solve ordinary differential equations of the RUnge-Kutta-Fehlberg Method with
 // variations in step size.
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main() {
+
+    FILE *output;
+    out1 = fopen("rk45.dat", "w");
+    
+    double h, t, s, s1, hmin, hmax;
+    double y[2], fReturn[2], ydumb[2];
+    
+}
