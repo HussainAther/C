135d76dd539ebace6f3991807490bc0f6c53a697
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 20:57:34 -0500

    define

--- tree.c
+++ tree.c
@@ -3,3 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+// if you don't have drand48, uncomment the following two lines
+//#define drand48 1.0/RAND_MAX*rand;
+//#define srand48 srand
+#define max 30000 // iterations
+#define seed 68111 // seed for rng
