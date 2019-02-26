82d39d51f2b19dad32874f53c7cccf7e7a73c1b2
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 21:06:31 -0500

    done

--- tree.c
+++ tree.c
@@ -11,10 +11,8 @@
 #define seed 68111 // seed for rng
 
 main() {
-    
     int i;
     double x, y ,r, xn, yn;
-    
     FILE *output;
     output = fopen("tree.dat", "w");
     srand48(seed);
@@ -22,21 +20,34 @@ main() {
     y = 0;
     for (i=1; i<=max; i++) {
         r = drand48();
-        if (r<=.1) {
+        if (r <= .1) {
             xn = .05 * x;
             yn = .5 * y;
         }
-    else if ((r > 0.1) && (r < 0.2)){
+    else if ((r > 0.1) && (r < 0.2)) {
         xn = .05 * x;
         yn = -.5 * y+1;
     }
-    else if ((r > 0.2) && (r < .04)) {
+    else if ((r > 0.2) && (r < .4)) {
         xn = .46 * x - .32 * y;
         yn = .39 * x + .38 * y + 6;
     }
-    else if ((r > 0.4) && (r < .06)) {
-        xn = .47 * x - .32 * y;
-        yn = .39 * x + .38 * y + 6;
+    else if ((r > 0.4) && (r < .6)) {
+        xn = .47 * x - .15 * y;
+        yn = .17 * x + .42 * y + 6;
     }
+    else if ((r > 0.6) && (r < .8)) {
+        xn = .43 * x + .28 * y;
+        yn = -.25 * x + .45 * y + 1;
     }
+    else {
+        xn = .42 * x + .26 * y;
+        yn = -.35 * x + .31 * y + .7;
+    }
+    fprintf(output, "%f %f\n", xn, yn);
+    x = xn;
+    y = yn;
+    }
+    printf("data stored in tree.dat\n");
+    fclose(output);
 }
