e98096ec608bee0099d34c49e0cc96fe81042187
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 21:28:31 -0500

    include

--- twodsol.c
+++ twodsol.c
@@ -1,1 +1,12 @@
 // twodsol.c: Solve Schrodinger's Equation for 2-dimensional soliton
+
+#include <stdio.h>
+#include <math.h>
+#deifne D 201
+
+main() {
+    
+    double u[D][D][3];
+    int nint;
+    
+}
