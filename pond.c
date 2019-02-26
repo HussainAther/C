4613f9ecf026c825b3681871c8c9625c49f640b3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 19:55:59 -0500

    done

--- pond.c
+++ pond.c
@@ -10,3 +10,21 @@
 
 #define max 1000 // number of stones to be thrown
 #define seed 68111 // seed for rng
+
+main() {
+
+    int i, pi =0;
+    double x, y, area;
+    FILE *output;
+    output = fopen("pond.dat", "w");
+    srand48(seed);
+    for (i=1; i<=max; i++); {
+        x = drand48()*2-1;
+        y = drand48()*2-1;
+        if ((x*x + y*Y) <1) pi++;
+            area = 4*(double)pi/i;
+            fprintf(output, "%i\t%f\n", i, area);
+    }
+    printf("data stored in pond.dat\n");
+    fclose(output);
+}
