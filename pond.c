40b33250a5ae394fb4e2011dd671137090a43e83
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 19:53:49 -0500

    uncomment

--- pond.c
+++ pond.c
@@ -1,4 +1,9 @@
 // pond.c: Monte Carlo integration to determine pi
 // (stone throwing, sampling, or Buffon's needle method)
 
+#include <stdio.h>
+#include <stdlib.h>
 
+// if you don't have drand48, uncomment the following two lines
+//#define drand48 1.0/RAND_MAX*rand
+//#define srand48 srand
