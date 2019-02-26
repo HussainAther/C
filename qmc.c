ac403c63ae8c74d47229ff51b1786f91e06685a3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 20:02:38 -0500

    main

--- qmc.c
+++ qmc.c
@@ -20,4 +20,24 @@ main() {
     for (j=0; j<=100; j++) path[j] = 0; // initial path
     for (j=0; j<=100; j++) prop[j] = 0; // initial probability
     oldE = energy(path); // energy of the path
+    
+    for (i=0; i<max; i++) {
+    element = drand48()*101;
+    change = (int)((drand48() -.5)*20)/10
+    path[element] += change;
+    newE = energy(path);
+    
+    if ((newE>oldE) &&(exp(-newE+oldE) <= drand48()))
+        { path[element] -= change; }
+    
+    for (j=0; j<=100; j++) {
+        element=path[j]*10+50;
+        prop[element]++;
+    }
+    oldE = newE;
+    }
+    for (i=0; i<=100; i++)
+    {fprint(output, "%d\t%f\n", i-50, (double) prop[i]/max);}
+    printf("data stored in qmc.dat\n");
+    fclose(output)
 }
