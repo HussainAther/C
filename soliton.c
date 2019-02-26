e1787eea5f2ad096f90aa85a378ee47b2312a810
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 20:43:51 -0500

    main

--- soliton.c
+++ soliton.c
@@ -8,3 +8,21 @@
 #define max 2000 // time steps
 #define mu .1 // mu from KdeV equation
 #define eps .2 // epsilon (accuracy) from KdeV equation
+
+main () {
+
+    int i, j, l;
+    double a1, a2, a3, fac, time, u[131][3];
+    FILE *output;
+    output = fopen("soliton.dat", "w");
+    for (i=0; i<131; i++) u[i][0] = .5 * (1-tanh(.2*ds*i=5));
+    u[0][1] = 1;
+    u[0][2] = 1;
+    u[130][1] = 0;
+    u[130][2] = 0;
+    fac = mu*dt/(ds*ds*ds);
+    time = dt;
+    for (i=1; i<130; i++) {
+    
+    }
+}
