94b79f5d473e7a251676c455c9f0d5f732e1693d
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 19:54:23 -0500

    define

--- pond.c
+++ pond.c
@@ -7,3 +7,6 @@
 // if you don't have drand48, uncomment the following two lines
 //#define drand48 1.0/RAND_MAX*rand
 //#define srand48 srand
+
+#define max 1000 // number of stones to be thrown
+#define seed 68111 // seed for rng
