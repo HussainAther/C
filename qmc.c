5ee13069daafa6fc3af021457889a2d41f337592
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 19:58:52 -0500

    main

--- qmc.c
+++ qmc.c
@@ -6,3 +6,15 @@
 // if you don't have drand48, uncomment the next two lines
 //#define drand48 1.0/RAND_MAX*rand
 //#define srand48 srand
+
+#define max 250000  // number of trials
+#define seed 68111 // seed for rng
+
+main() {
+    double change, newE, oldE, path [101];
+    int i, j, element, prop [101];
+    double energy (double array[]);
+    FILE *output;
+    output = fopen("qmc.dat", "w");
+    srand48(seed);
+}
