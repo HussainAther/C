584da00ea2c4f2a9f58d2b9a9c1a8b2c70e25b08
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 19:57:48 -0500

    initial commit

---
+++ qmc.c
@@ -0,0 +1,8 @@
+// qmc.c: Feynman path integral for ground state wave function
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+// if you don't have drand48, uncomment the next two lines
+//#define drand48 1.0/RAND_MAX*rand
+//#define srand48 srand
