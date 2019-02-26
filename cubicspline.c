aa7ee3e344067530bb01527b7ebae23dcba95c16
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 21:22:39 -0500

    more for loops

--- cubicspline.c
+++ cubicspline.c
@@ -37,7 +37,15 @@ main() {
             p = sig*y2[i-1] + 2;
             y2[i] = (sig - 1)/p;
             u[i] = (y[i+1] - y[i])/(x[i+1] - x[i]) - (y[i] - y[i-1])/(x[i] - x[i-1]);
-            u[i] = (
-            
+            u[i] = (6*u[i]/(x[i+1]-x[i-1])-sig*u[i-1])/p;
         }
+    
+        if (ypn > .99e30) qn = un = 0;
+        else {
+            qn = .5;
+            un = ((3)/(x[n-1]-x[n-2])) * (ypn-y[n-1]-y[n-2])/(x[n-1]-x[n-2]);
+        }
+        y2[n-1] = (un-qn*u[n-2])/(qn*y2[n-2]+1);
+        for (k=n-2; k>=0; k--) {y2[k] = y2[k]*y2[k+1]+u[k];}
+        
 }
