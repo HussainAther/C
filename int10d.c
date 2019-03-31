391321fcd6706daeb184dc7a53b22d7b23c541e3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-30 21:05:19 -0400

    typo

--- int10d.c
+++ int10d.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-// if you don't haev drand48, uncomment hte following two lines
+// if you don't haev drand48, uncomment the following two lines
     #define drand48 1.0/RAND_MAX*rand
     #define srand48 srand
 #define max 65536 //number of trials
