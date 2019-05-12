ea8698a18577f71badf940210eddb7f37218899f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-12 09:30:28 -0400

    initial commit

---
+++ qsort.c
@@ -0,0 +1,13 @@
+# Sort a file of integers
+ int intcomp(int *x, int *y)
+      {   return *x - *y; }
+      int a[1000000];
+      int main(void)
+      {   int i, n=0;
+          while (scanf("%d", &a[n]) != EOF)
+              n++;
+          qsort(a, n, sizeof(int), intcomp);
+          for (i = 0; i < n; i++)
+              printf("%d\n", a[i]);
+          return 0;
+}
