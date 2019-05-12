229dec1dc09e8e3a69feccc63a31f68f56332de3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-12 09:41:40 -0400

    initial commit

---
+++ timemod.c
@@ -0,0 +1,12 @@
+/* Produce a table of C run time costs
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#define MAXN 100000
+int x[MAXN];
+int startn = 5000;
+int n;
+
+
